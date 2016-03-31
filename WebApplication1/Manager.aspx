1dfeef43fb97cb21f41109767be1438af786d0cf
Jing Liang <as458752@gmail.com>
2016-03-31 16:44:49 -0700

    First Submission
    Manager Page and Manager Login Page is finished

---
+++ WebApplication1/Manager.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Manager.aspx.cs" Inherits="PeerEvaluation.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="Label1" runat="server" Font-Size="XX-Large" Text="Manager Page"></asp:Label>
+        <br />
+        </div>
+        <p>
+            &nbsp;</p>
+        <asp:Panel ID="Panel2" runat="server" Height="232px" HorizontalAlign="Center" style="margin-left: 300px" Width="585px">
+            <asp:Label ID="Label3" runat="server" Text="Professor Name"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label4" runat="server" Text="ASU ID"></asp:Label>
+            <br />
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server" Height="135px"></asp:ListBox>
+            <br />
+            <br />
+            <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+        </asp:Panel>
+        <p>
+        </p>
+        <asp:Panel ID="Panel1" runat="server" Height="139px" HorizontalAlign="Center" style="margin-left: 300px" Width="585px">
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Name:"></asp:Label>
+            &nbsp;<asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            ASU ID:
+            <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnAdd" runat="server" OnClick="btnAdd_Click" Text="Add" Width="56px" />
+        </asp:Panel>
+        <br />
+        <hr />
+        <br />
+        <asp:Panel ID="Panel3" runat="server" Height="182px" HorizontalAlign="Center" style="margin-left: 300px" Width="585px">
+            <br />
+            <asp:Label ID="Label6" runat="server" Text="Old Password:"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtPasswordOld" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+            New Password:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtPasswordNew" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+            Confirm Password:
+            <asp:TextBox ID="txtPasswordConfirm" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnChange" runat="server" OnClick="btnChange_Click" Text="Change Password" />
+            <br />
+            <br />
+            <asp:Label ID="lblResult" runat="server" ForeColor="#FF3300" Text="Label" Visible="False"></asp:Label>
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Manager.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PeerEvaluation
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private string fLocation;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["manager"] == null)
+            {
+                Response.Redirect("ManagerLogin.aspx");
+            }
+            else
+            {
+                fLocation = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data\professor.txt");
+                if (!IsPostBack)
+                {
+                    string line;
+                    System.IO.StreamReader file = new System.IO.StreamReader(fLocation);
+                    string[] lines = System.IO.File.ReadAllLines(fLocation);
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        ListBox1.Items.Add(line);
+                    }
+                    file.Close();
+                }
+            }
+        }
+
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            int idx = ListBox1.SelectedIndex;
+            ListBox1.Items.RemoveAt(idx);
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(fLocation);
+            foreach (var item in ListBox1.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+
+            SaveFile.Close();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string spaces = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+            spaces = Server.HtmlDecode(spaces);
+            string newLine = txtName.Text + spaces + txtId.Text;
+            ListBox1.Items.Add(newLine);
+            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(fLocation);
+            foreach (var item in ListBox1.Items)
+            {
+                SaveFile.WriteLine(item);
+            }
+            SaveFile.Close();
+            txtName.Text = String.Empty;
+            txtId.Text = String.Empty;
+        }
+
+        protected void btnChange_Click(object sender, EventArgs e)
+        {
+            if(txtPasswordOld.Text == "" || txtPasswordNew.Text == "" || txtPasswordConfirm.Text == "") lblResult.Text = "Password can not be empty!";
+            else
+            {
+                if (txtPasswordNew.Text != txtPasswordConfirm.Text) lblResult.Text = "New passwords do not match!";
+                else
+                {
+                    string line;
+                    string fileLoc = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data\managerPassword.txt");
+                    System.IO.StreamReader file = new System.IO.StreamReader(fileLoc);
+                    if ((line = file.ReadLine()) != null)
+                    {
+                        file.Close();
+                        line = Encryption.decrypt(line, 4);
+                        if (txtPasswordOld.Text != line) lblResult.Text = "Old passwords do not match!";
+                        else
+                        {
+                            System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(fileLoc);
+                            SaveFile.WriteLine(Encryption.encrypt(txtPasswordNew.Text,4));
+                            lblResult.Text = "Password has been changed!";
+                            SaveFile.Close();
+                        }
+                    }
+                    else
+                    {
+                        lblResult.Text = "System file is damaged!";
+                        file.Close();
+                    }
+                }
+            }
+            lblResult.Visible = true;
+        }
+
+        
+    }
+}
